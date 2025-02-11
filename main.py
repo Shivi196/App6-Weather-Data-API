65ff63a7d8c7d98c0169786f4aa3a98729e15570
misha <misha07.sharma@gmail.com>
2025-02-11 17:40:22 +0530

    Day30: Show stations table on home page

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from ast import parse
-
 from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("/Users/misha/PycharmProjects/App6--Weather-API/data_small/stations.txt",skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html",data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
